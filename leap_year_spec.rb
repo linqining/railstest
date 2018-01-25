037779da24eed14a6a9202103d9ce1c5fd735010
linqining <linqining@outlook.com>
2018-01-25 14:17:43 +0800

    Rails test first commit

---
+++ leap_year_spec.rb
@@ -0,0 +1,20 @@
+require_relative './leap_year'
+
+describe "Leap Year" do
+  it "2016 is leap year" do
+    result=is_leap_year?(2016)
+    expect(result).to eq(true)
+  end
+  it "2017 is leap year" do
+    result = is_leap_year?(2017)
+    expect(result).to eq(false)
+  end
+  it "2100 is common year" do
+    result = is_leap_year?(2100)
+    expect(result).to eq(false)
+  end
+  it "2400 is common year" do
+    result = is_leap_year?(2400)
+    expect(result).to eq(true)
+  end
+end
